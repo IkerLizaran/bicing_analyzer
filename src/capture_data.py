80fa189e2fbf9862e11473612202ff5b12587e5d
Iker Lizaran <ikerlizaran@MacBook-Air-de-Iker-3.local>
2026-08-03 03:03:17 +0200

    Simplify capture script to use database only (fixes GitHub Actions run)

--- src/capture_data.py
+++ src/capture_data.py
@@ -1,11 +1,8 @@
 from logger_config import setup_logging
 from transform import merge_df, save_snapshot, save_to_db
 
-setup_logging()
-
 def main():
     df = merge_df()
     save_to_db(df)
-    save_snapshot(df)
 
 main()
